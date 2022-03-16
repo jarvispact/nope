1391ab4c8fab589e8e6872e4ac7b931d460261e7
jarvispact <jarvispact@gmail.com>
2022-03-16 16:29:14 +0100

    feat: remove test-utils from dist folder

--- dist/test-utils.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="chai" />
-export declare const tsExpect: <Expectation, Expected extends Expectation>(expectation: Expectation, expected: Expected) => Chai.Assertion;
--- dist/test-utils.js
+++
@@ -1,3 +0,0 @@
-import { expect } from 'chai';
-export const tsExpect = (expectation, expected) => expect(expectation).to.eql(expected);
-//# sourceMappingURL=test-utils.js.map
