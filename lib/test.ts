37c5a20319b0dd1ea92444e01f07b0e531d2bb99
jarvispact <jarvispact@gmail.com>
2022-08-06 22:31:35 +0200

    feat: basic functionality

---
+++ lib/test.ts
@@ -0,0 +1,218 @@
+import {
+    createError,
+    Either,
+    failure,
+    isFailure,
+    isObject,
+    isSuccess,
+    objectKeys,
+    Opaque,
+    Schema,
+    schema,
+    SchemaError,
+    success,
+} from './utils';
+
+export const stringSchema = schema<
+    'string',
+    string,
+    string,
+    SchemaError<'string', 'E_STRING'>
+>({
+    uri: 'string',
+    is: (input) => typeof input === 'string',
+    validate: (input, { uri, is }) =>
+        is(input)
+            ? success(input)
+            : failure(
+                  createError(
+                      uri,
+                      'E_STRING',
+                      `input: "${input}" is not of type string`,
+                  ),
+              ),
+});
+
+export type Email = Opaque<string, 'Email'>;
+
+export const emailSchema = schema<
+    'email',
+    string,
+    Email,
+    SchemaError<'email', 'E_EMAIL'>
+>({
+    uri: 'email',
+    is: (input) => stringSchema.is(input) && input.includes('@'),
+    validate: (input, { uri, is }) =>
+        is(input)
+            ? success(input)
+            : failure(
+                  createError(
+                      uri,
+                      'E_EMAIL',
+                      `input: "${input}" is not of type email`,
+                  ),
+              ),
+});
+
+type ArraySchemaError<
+    ItemSchema extends Schema<string, unknown, unknown, unknown>,
+> = {
+    error: SchemaError<'array', 'E_ARRAY'> | null;
+    items: Either<ItemSchema['O'], ItemSchema['E']>[];
+};
+
+type ArraySchema<ItemSchema extends Schema<string, unknown, unknown, unknown>> =
+    {
+        uri: 'array';
+        I: ItemSchema['I'][];
+        O: ItemSchema['O'][];
+        E: ArraySchemaError<ItemSchema>;
+        is: (input: ItemSchema['I'][]) => input is ItemSchema['O'][];
+        validate: (
+            input: ItemSchema['I'][],
+        ) => Either<ItemSchema['O'][], ArraySchemaError<ItemSchema>>;
+        collectErrors: (
+            input: ItemSchema['I'][],
+        ) => (SchemaError<'array', 'E_ARRAY'> | ItemSchema['E'])[];
+    };
+
+export const arraySchema = <
+    ItemSchema extends Schema<string, unknown, unknown, unknown>,
+>(
+    itemSchema: ItemSchema,
+): ArraySchema<ItemSchema> => {
+    const _schema = schema<
+        'array',
+        ItemSchema['I'][],
+        ItemSchema['O'][],
+        ArraySchemaError<ItemSchema>
+    >({
+        uri: 'array',
+        is: (arrayInput) =>
+            Array.isArray(arrayInput) ? arrayInput.every(itemSchema.is) : false,
+        validate: (arrayInput, { uri, is }) => {
+            if (is(arrayInput)) return success(arrayInput);
+            if (!Array.isArray(arrayInput))
+                return failure({
+                    error: createError(
+                        uri,
+                        'E_ARRAY',
+                        `input: "${arrayInput}" is not of type array`,
+                    ),
+                    items: [],
+                });
+            return failure({
+                error: null,
+                items: arrayInput.map(itemSchema.validate),
+            });
+        },
+    });
+
+    return {
+        ..._schema,
+        collectErrors: (input) => {
+            const either = _schema.validate(input);
+            if (isSuccess(either)) return [];
+            return [
+                ...(either.value.error ? [either.value.error] : []),
+                ...either.value.items.filter(isFailure),
+            ];
+        },
+    };
+};
+
+type RecordSchemaError<
+    Definition extends {
+        [Key: string]: Schema<string, unknown, unknown, unknown>;
+    },
+> = {
+    error: SchemaError<'record', 'E_RECORD'> | null;
+    properties: {
+        [K in keyof Definition]: Either<Definition[K]['O'], Definition[K]['E']>;
+    };
+};
+
+type RecordSchema<
+    Definition extends {
+        [Key: string]: Schema<string, unknown, unknown, unknown>;
+    },
+> = {
+    uri: 'record';
+    I: { [K in keyof Definition]: Definition[K]['I'] };
+    O: { [K in keyof Definition]: Definition[K]['O'] };
+    E: RecordSchemaError<Definition>;
+    is: (input: { [K in keyof Definition]: Definition[K]['I'] }) => input is {
+        [K in keyof Definition]: Definition[K]['O'];
+    };
+    validate: (input: {
+        [K in keyof Definition]: Definition[K]['I'];
+    }) => Either<
+        { [K in keyof Definition]: Definition[K]['O'] },
+        RecordSchemaError<Definition>
+    >;
+    collectErrors: (input: { [K in keyof Definition]: Definition[K]['I'] }) => (
+        | SchemaError<'record', 'E_RECORD'>
+        | Definition[string]['E']
+    )[];
+};
+
+export const recordSchema = <
+    Definition extends {
+        [Key: string]: Schema<string, unknown, unknown, unknown>;
+    },
+>(
+    definition: Definition,
+): RecordSchema<Definition> => {
+    const _schema = schema<
+        'record',
+        { [K in keyof Definition]: Definition[K]['I'] },
+        { [K in keyof Definition]: Definition[K]['O'] },
+        RecordSchemaError<Definition>
+    >({
+        uri: 'record',
+        is: (input) =>
+            isObject(input) &&
+            objectKeys(definition).every((defKey) =>
+                definition[defKey].is(input[defKey]),
+            ),
+        validate: (input, { uri, is }) => {
+            if (is(input)) return success(input);
+            if (!isObject(input))
+                return failure({
+                    error: createError(
+                        uri,
+                        'E_RECORD',
+                        `input: "${input}" is not of type record`,
+                    ),
+                    properties: {} as {
+                        [K in keyof Definition]: Either<
+                            Definition[K]['O'],
+                            Definition[K]['E']
+                        >;
+                    },
+                });
+            return failure({
+                error: null,
+                properties: objectKeys(definition).reduce((accum, defKey) => {
+                    accum[defKey] = definition[defKey].validate(input[defKey]);
+                    return accum;
+                }, {} as { [K in keyof Definition]: Either<Definition[K]['O'], Definition[K]['E']> }),
+            });
+        },
+    });
+
+    return {
+        ..._schema,
+        collectErrors: (input) => {
+            const either = _schema.validate(input);
+            if (isSuccess(either)) return [];
+            return [
+                ...(either.value.error ? [either.value.error] : []),
+                ...objectKeys(either.value.properties)
+                    .filter((key) => isFailure(either.value.properties[key]))
+                    .map((key) => either.value.properties[key]),
+            ];
+        },
+    };
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+export const objectKeys = <T extends { [x: string]: unknown }>(rec: T) =>
+    Object.keys(rec) as Array<keyof T>;
+
+export const isObject = (v: unknown): v is Record<string, unknown> =>
+    typeof v === 'object' &&
+    !Array.isArray(v) &&
+    v !== null &&
+    !(v instanceof Date);
 
 export type Success<T> = { status: 'SUCCESS'; value: T };
 export type Failure<T> = { status: 'FAILURE'; value: T };
@@ -24,273 +31,6 @@ export const isSuccess = <S, F>(either: Either<S, F>): either is Success<S> =>
 export const isFailure = <S, F>(either: Either<S, F>): either is Failure<F> =>
     either.status === 'FAILURE';
 
-export const createError = <
-    Uri extends string,
-    Code extends string,
-    Details extends { [Key: string]: unknown },
->(
-    uri: Uri,
-    code: Code,
-    message: string,
-    details: Details = {} as Details,
-) => ({
-    uri,
-    code,
-    message,
-    details,
-});
-
-export type Schema<Input, Output extends Input, Err, Uri extends string> = {
-    I: Input;
-    O: Output;
-    E: Err;
-    uri: Uri;
-    is: (input: Input) => input is Output;
-    create: (input: Input) => Output;
-    validate: (input: Input) => Either<Output, Err>;
-    serialize: () => string;
-};
-
-type CreateSchemaPropsWithValidateFunction<
-    Input,
-    Output extends Input,
-    Err,
-    Uri extends string,
-> = {
-    uri: Uri;
-    is: (input: Input) => input is Output;
-    create: (input: Input) => Output;
-    validate: (
-        input: Input,
-        ctx: {
-            uri: Uri;
-            is: (input: Input) => input is Output;
-            create: (input: Input) => Output;
-            serialize: () => string;
-        },
-    ) => Either<Output, Err>;
-    serialize?: () => string;
-};
-
-type CreateSchemaPropsWithErrorConstructor<
-    Input,
-    Output extends Input,
-    Err,
-    Uri extends string,
-> = {
-    uri: Uri;
-    is: (input: Input) => input is Output;
-    create: (input: Input) => Output;
-    err: (input: Input) => Err;
-    serialize?: () => string;
-};
-
-export const createSchema = <
-    Input,
-    Output extends Input,
-    Err,
-    Uri extends string,
->({
-    uri,
-    is,
-    create,
-    validate,
-    serialize,
-}: CreateSchemaPropsWithValidateFunction<Input, Output, Err, Uri>): Schema<
-    Input,
-    Output,
-    Err,
-    Uri
-> => {
-    const I = null as unknown as Input;
-    const O = null as unknown as Output;
-    const E = null as unknown as Err;
-
-    const defaultserialize = () => uri;
-
-    return {
-        I,
-        O,
-        E,
-        uri,
-        is,
-        create,
-        validate: (input) =>
-            validate(input, {
-                uri,
-                is,
-                create,
-                serialize: serialize || defaultserialize,
-            }),
-        serialize: serialize || defaultserialize,
-    };
-};
-
-type ArrayElement<ArrayType> = ArrayType extends readonly (infer ElementType)[]
-    ? ElementType
-    : ArrayType;
-
-type ExtendSchemaOverload = {
-    <
-        S extends Schema<any, any, any, any>,
-        Input extends S['I'],
-        Output extends Input,
-        Err,
-        Uri extends string,
-    >(
-        schema: S,
-        {
-            uri,
-            is,
-            create,
-            validate,
-        }: CreateSchemaPropsWithValidateFunction<Input, Output, Err, Uri>,
-    ): Schema<
-        Input,
-        Output,
-        Array<ArrayElement<Err> | ArrayElement<S['E']>>,
-        Uri
-    >;
-    <
-        S extends Schema<any, any, any, any>,
-        Input extends S['I'],
-        Output extends Input,
-        Err,
-        Uri extends string,
-    >(
-        schema: S,
-        {
-            uri,
-            is,
-            create,
-            err,
-        }: CreateSchemaPropsWithErrorConstructor<Input, Output, Err, Uri>,
-    ): Schema<Input, Output, Array<Err | ArrayElement<S['E']>>, Uri>;
-};
-
-export const extendSchema: ExtendSchemaOverload = <
-    S extends Schema<any, any, any, any>,
-    Input extends S['I'],
-    Output extends Input,
-    Err,
-    Uri extends string,
->(
-    schema: S,
-    {
-        uri,
-        is,
-        create,
-        err,
-        validate,
-    }: {
-        uri: Uri;
-        is: (input: Input) => input is Output;
-        create: (input: Input) => Output;
-        err?: (input: Input) => Array<Err | ArrayElement<S['E']>>;
-        validate?: (
-            input: Input,
-            ctx: {
-                uri: Uri;
-                is: (input: Input) => input is Output;
-                create: (input: Input) => Output;
-                serialize: () => string;
-            },
-        ) => Either<Output, Array<Err | ArrayElement<S['E']>>>;
-    },
-) => {
-    if (err) {
-        return extendSchemaWithErrorConstructor(schema, {
-            uri,
-            is,
-            create,
-            err,
-        });
-    } else if (validate) {
-        return extendSchemaWithValidateFunction(schema, {
-            uri,
-            is,
-            create,
-            validate,
-        });
-    }
-    throw new Error('you need to provide a "err" or "validate" function');
-};
-
-const extendSchemaWithValidateFunction = <
-    S extends Schema<any, any, any, any>,
-    Input extends S['I'],
-    Output extends Input,
-    Err,
-    Uri extends string,
->(
-    schema: S,
-    {
-        uri,
-        is,
-        create,
-        validate,
-    }: CreateSchemaPropsWithValidateFunction<Input, Output, Err, Uri>,
-) => {
-    return createSchema({
-        uri,
-        is: (input): input is Output => schema.is(input) && is(input),
-        create,
-        validate: (input, ctx) => {
-            const either = schema.validate(input);
-            const result = validate(input, ctx);
-
-            const errors = [either, result]
-                .filter((e) => e.status === 'FAILURE')
-                .flatMap((e) => e.value) as Array<
-                ArrayElement<Err> | ArrayElement<S['E']>
-            >;
-
-            if (errors.length) return failure(errors);
-
-            return success(input);
-        },
-    });
-};
-
-const extendSchemaWithErrorConstructor = <
-    S extends Schema<any, any, any, any>,
-    Input extends S['I'],
-    Output extends Input,
-    Err,
-    Uri extends string,
->(
-    schema: S,
-    {
-        uri,
-        is,
-        create,
-        err,
-    }: CreateSchemaPropsWithErrorConstructor<Input, Output, Err, Uri>,
-) => {
-    return createSchema({
-        uri,
-        is: (input): input is Output => schema.is(input) && is(input),
-        create,
-        validate: (input, { is }) => {
-            const either = schema.validate(input);
-
-            const result = is(input)
-                ? success(create(input))
-                : failure(err(input));
-
-            const errors = [either, result]
-                .filter((e) => e.status === 'FAILURE')
-                .flatMap((e) => e.value) as Array<Err | ArrayElement<S['E']>>;
-
-            if (errors.length) return failure(errors);
-
-            return success(input);
-        },
-    });
-};
-
-export const identity = <T>(val: T) => val;
-
 declare const tag: unique symbol;
 
 declare type Tagged<Token> = {
@@ -299,29 +39,55 @@ declare type Tagged<Token> = {
 
 export type Opaque<Type, Token = unknown> = Type & Tagged<Token>;
 
-export const objectKeys = <T extends { [x: string]: unknown }>(rec: T) =>
-    Object.keys(rec) as Array<keyof T>;
-
-export const isObject = (v: unknown): v is Record<string, unknown> =>
-    typeof v === 'object' && !Array.isArray(v) && v !== null;
-
-export const getDisplayType = (value: unknown) => {
-    if (value === null) return 'null';
-    if (value instanceof Date) return 'date';
-    if (isObject(value)) return 'record';
-    if (Array.isArray(value)) return 'array';
-    return typeof value;
+export type SchemaError<Uri extends string, Code extends string> = {
+    uri: Uri;
+    code: Code;
+    message: string;
 };
 
-export const getErrorDetails = <T extends string>(
-    expectedType: T,
-    input: unknown,
-) => ({
-    expectedType,
-    providedType: getDisplayType(input),
-    providedNativeType: typeof input,
-    providedValue: input,
-});
+export type ArrayItem<ArrayType> =
+    ArrayType extends readonly (infer ElementType)[] ? ElementType : ArrayType;
 
-export const isNotNil = <T>(val: T | undefined | null): val is T =>
-    val !== undefined && val !== null;
+export const createError = <Uri extends string, Code extends string>(
+    uri: Uri,
+    code: Code,
+    message: string,
+): SchemaError<Uri, Code> => ({ uri, code, message });
+
+type ValidateContext<Uri extends string, I, O extends I> = {
+    uri: Uri;
+    is: (input: I) => input is O;
+};
+
+type SchemaArgs<Uri extends string, I, O extends I, E> = {
+    uri: Uri;
+    is: (input: I) => boolean;
+    validate: (input: I, ctx: ValidateContext<Uri, I, O>) => Either<O, E>;
+};
+
+export type Schema<Uri extends string, I, O extends I, E> = {
+    uri: Uri;
+    I: I;
+    O: O;
+    E: E;
+    is: (input: I) => input is O;
+    validate: (input: I) => Either<O, E>;
+};
+
+export const schema = <Uri extends string, I, O extends I, E>({
+    uri,
+    is,
+    validate,
+}: SchemaArgs<Uri, I, O, E>): Schema<Uri, I, O, E> => {
+    const _is = (input: I): input is O => is(input);
+    const _validate = (input: I) => validate(input, { is: _is, uri });
+
+    return {
+        uri,
+        I: null as unknown as I,
+        O: null as unknown as O,
+        E: null as unknown as E,
+        is: _is,
+        validate: _validate,
+    };
+};
