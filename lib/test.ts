e341898ed3b4f43a988cd100f78c011d4780be82
jarvispact <jarvispact@gmail.com>
2022-08-07 12:59:12 +0200

    feat: working collectErrors type

--- lib/test.ts
+++ lib/test.ts
@@ -4,9 +4,7 @@ import {
     Either,
     Failure,
     failure,
-    isFailure,
     isObject,
-    isSuccess,
     objectKeys,
     Opaque,
     Schema,
@@ -36,6 +34,123 @@ export const stringSchema = schema<
               ),
 });
 
+export type StringSchema = typeof stringSchema;
+
+export const numberSchema = schema<
+    'number',
+    number,
+    number,
+    SchemaError<'number', 'E_NUMBER'>
+>({
+    uri: 'number',
+    is: (input) => typeof input === 'number',
+    validate: (input, { uri, is }) =>
+        is(input)
+            ? success(input)
+            : failure(
+                  createError(
+                      uri,
+                      'E_NUMBER',
+                      `input: "${input}" is not of type number`,
+                  ),
+              ),
+});
+
+export type NumberSchema = typeof numberSchema;
+
+export const booleanSchema = schema<
+    'boolean',
+    boolean,
+    boolean,
+    SchemaError<'boolean', 'E_BOOLEAN'>
+>({
+    uri: 'boolean',
+    is: (input) => typeof input === 'boolean',
+    validate: (input, { uri, is }) =>
+        is(input)
+            ? success(input)
+            : failure(
+                  createError(
+                      uri,
+                      'E_BOOLEAN',
+                      `input: "${input}" is not of type boolean`,
+                  ),
+              ),
+});
+
+export type BooleanSchema = typeof booleanSchema;
+
+export const stringLiteralSchema = <Literal extends string>(literal: Literal) =>
+    schema<
+        'string-literal',
+        string,
+        Literal,
+        SchemaError<'string-literal', 'E_STRING_LITERAL'>
+    >({
+        uri: 'string-literal',
+        is: (input) => input === literal,
+        validate: (input, { uri, is }) =>
+            is(input)
+                ? success(input)
+                : failure(
+                      createError(
+                          uri,
+                          'E_STRING_LITERAL',
+                          `input: "${input}" is not of type ${uri}(${literal})`,
+                      ),
+                  ),
+    });
+
+export type StringLiteralSchema = typeof stringLiteralSchema;
+
+export const numberLiteralSchema = <Literal extends number>(literal: Literal) =>
+    schema<
+        'number-literal',
+        number,
+        Literal,
+        SchemaError<'number-literal', 'E_NUMBER_LITERAL'>
+    >({
+        uri: 'number-literal',
+        is: (input) => input === literal,
+        validate: (input, { uri, is }) =>
+            is(input)
+                ? success(input)
+                : failure(
+                      createError(
+                          uri,
+                          'E_NUMBER_LITERAL',
+                          `input: "${input}" is not of type ${uri}(${literal})`,
+                      ),
+                  ),
+    });
+
+export type NumberLiteralSchema = typeof numberLiteralSchema;
+
+export const booleanLiteralSchema = <Literal extends boolean>(
+    literal: Literal,
+) =>
+    schema<
+        'boolean-literal',
+        boolean,
+        Literal,
+        SchemaError<'boolean-literal', 'E_BOOLEAN_LITERAL'>
+    >({
+        uri: 'boolean-literal',
+        is: (input) => input === literal,
+        validate: (input, { uri, is }) =>
+            is(input)
+                ? success(input)
+                : failure(
+                      createError(
+                          uri,
+                          'E_BOOLEAN_LITERAL',
+                          `input: "${input}" is not of type ${uri}(${literal})`,
+                      ),
+                  ),
+    });
+
+export type BooleanLiteralSchema = typeof booleanLiteralSchema;
+
 export type Email = Opaque<string, 'Email'>;
 
 export const emailSchema = schema<
@@ -57,6 +172,8 @@ export const emailSchema = schema<
                   ),
               ),
 });
+
+export type EmailSchema = typeof emailSchema;
 
 type ArraySchemaError<ItemSchema extends Schema<any, any, any, any>> = {
     error: SchemaError<'array', 'E_ARRAY'> | null;
@@ -123,13 +240,30 @@ type RecordSchemaError<
 type RemoveNull<T> = T extends null ? never : T;
 
 type ObjectValues<T extends { [K: string]: any }> = T[keyof T][];
-type F = ObjectValues<{ firstname: string; age: number }>;
-
-// try to use a Object.values approach with conditionals
 
 type TakeFailure<T extends Either<any, any>> = T extends Success<T['value']>
     ? never
     : T['value'];
+
+type CollectRecordProperties<T extends { [K: string]: Either<any, any> }> =
+    ObjectValues<{
+        [K in keyof T]: TakeFailure<T[K]> extends RecordSchemaError<{
+            [Key: string]: Schema<any, any, any, any>;
+        }>
+            ? CollectRecordProperties<TakeFailure<T[K]>['properties']>
+            : TakeFailure<T[K]>;
+    }>[number];
+
+type CollectErrors<
+    T extends Failure<
+        RecordSchemaError<{
+            [Key: string]: Schema<any, any, any, any>;
+        }>
+    >,
+> = (
+    | RemoveNull<T['value']['error']>
+    | CollectRecordProperties<T['value']['properties']>
+)[];
 
 type RecordSchema<
     Definition extends {
@@ -140,6 +274,7 @@ type RecordSchema<
     I: { [K in keyof Definition]: Definition[K]['I'] };
     O: { [K in keyof Definition]: Definition[K]['O'] };
     E: RecordSchemaError<Definition>;
+    definition: Definition;
     is: (input: { [K in keyof Definition]: Definition[K]['I'] }) => input is {
         [K in keyof Definition]: Definition[K]['O'];
     };
@@ -151,12 +286,7 @@ type RecordSchema<
     >;
     collectErrors: (
         failure: Failure<RecordSchemaError<Definition>>,
-    ) => (
-        | RemoveNull<RecordSchemaError<Definition>['error']>
-        | TakeFailure<
-              RecordSchemaError<Definition>['properties'][keyof RecordSchemaError<Definition>['properties']]
-          >
-    )[];
+    ) => CollectErrors<Failure<RecordSchemaError<Definition>>>;
 };
 
 export const recordSchema = <
@@ -205,6 +335,7 @@ export const recordSchema = <
 
     return {
         ..._schema,
+        definition,
         collectErrors: () => null as any,
     };
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import {
     arraySchema,
+    booleanSchema,
     emailSchema,
+    stringLiteralSchema,
+    numberSchema,
     recordSchema,
     stringSchema,
+    numberLiteralSchema,
+    booleanLiteralSchema,
 } from '../lib/test';
 import { isFailure } from '../lib/utils';
 import './style.css';
@@ -11,15 +16,30 @@ const PersonSchema = recordSchema({
     firstname: stringSchema,
     email: emailSchema,
     // friends: arraySchema(emailSchema),
+    nested: recordSchema({
+        age: numberSchema,
+        nested2: recordSchema({
+            what: booleanSchema,
+            nested3: recordSchema({
+                bar: stringLiteralSchema('abc'),
+            }),
+            nested4: recordSchema({
+                baz: numberLiteralSchema(42),
+                nested5: recordSchema({
+                    baz: booleanLiteralSchema(false),
+                }),
+            }),
+        }),
+    }),
 });
 
-const either = PersonSchema.validate({} as any);
-if (isFailure(either)) {
-    const errors = PersonSchema.collectErrors(either);
-    const item = errors[0];
+const res = PersonSchema.validate({} as any);
+if (isFailure(res)) {
+    const errors = PersonSchema.collectErrors(res);
+    const item = errors[0].code;
 }
 
-console.log({ either });
+console.log({ either: res });
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const app = document.querySelector<HTMLDivElement>('#app')!;
